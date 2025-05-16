fcaf35628fe18ea8bbfec9229ff19c47bcdceee9
Kauan <kauan.mariani@gmail.com>
2025-05-16 16:52:58 -0300

    esboço de uma primeira página

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,53 @@
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet">
+
+<header>
+<h1>Reinforcement Learning</h1>
+</header>
+
+<slot></slot>
+
+
+<style>
+    :global(body) {
+        margin: 0;
+        padding: 0;
+        background-color: black;
+        color: white;
+
+    }
+
+    header {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+
+    h1 {
+        font-family: "Press Start 2P", sans-serif;
+        font-size: 1.5rem;
+        text-align: center;
+        color: white;
+        white-space: nowrap;
+        overflow: hidden;
+        border-right: 2px solid white;
+        width: 0; 
+        animation: typing 3s steps(22, end) forwards,
+                   blink 0.7s step-end infinite,
+                   shake 2s infinite;
+    }
+
+
+
+
+    @keyframes typing {
+      from { width: 0 }
+      to { width: 22ch } /* 26 characters in "Reinforcement Learning" */
+    }
+
+    @keyframes blink {
+      50% { border-color: transparent }
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,82 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import { goto } from '$app/navigation';
+
+    function handleFirstAction() {
+      goto("/explained");
+      
+    }
+    
+    function handleSecondAction() {
+        goto("/visualization");
+    }
+  </script>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400..700;1,400..700&family=Press+Start+2P&display=swap" rel="stylesheet">
+  <div class="center-box">
+    <div class="text-box">
+        Reinforcement Learning é uma subárea do aprendizado de máquina, a qual difere. 
+        Com o objetivo de desmistificar o que é tal aprendizado, decidimos mostrar como esse aprendizado ocorre. 
+        Aqui você conseguirá tanto aprender de um jeito dinâmico o que é o  reinforcement learning
+        quanto entender interativamente como ele funciona, podendo brincar com um toy model e contorlando os parâmentros. 
+    </div>
+
+    <div class="button-group">
+      <button on:click={handleFirstAction}>O que é Reinforcement Learning?</button>
+      <button on:click={handleSecondAction}>Aprender interativamente</button>
+    </div>
+  </div>
+  
+  <style>
+    /* Centraliza tudo no meio da tela */
+    .center-box {
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      background-color: #444;
+      color: white;
+      padding: 30px;
+      border-radius: 16px;
+      box-shadow: 0 0 15px rgba(255, 255, 255, 0.2);
+      text-align: center;
+      min-height: 40vh;
+      min-width: 60vw;
+    }
+    
+    .text-box {
+        font-family: "Lato", sans-serif;
+        text-align: justify;
+        margin-left: 5rem;
+        margin-right: 5rem;
+        margin-bottom: 1rem;
+        font-size: 1.5rem;
+    }
+    /* .center-box h2 {
+      font-family: "Lato", sans-serif;
+      font-size: 2rem;
+      margin-bottom: 20px;
+    } */
+  
+    .button-group {
+      display: flex;
+      gap: 15px;
+      justify-content: center;
+    }
+  
+    button {
+      font-family: "Lato", sans-serif;
+      background-color: #444;
+      color: white;
+      border: 2px solid white;
+      padding: 10px 20px;
+      cursor: pointer;
+      transition: background-color 0.3s ease;
+      font-size: 1rem;
+    }
+  
+    button:hover {
+      background-color: #666;
+    }
+  </style>
+  
